d7172233eabef100ad9831ad18858090151cba4f
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-04-02 11:31:19 +0530

    Add files via upload

---
+++ Basic/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3);
+console.log("Nil");
+
+let age = 22
+let name = "Nil"
+let isLoggedIn = true
+let state 
+
+/*
+number = upto 2^53
+bigint
+string = ""
+boolean = true/false
+null = standalone value
+undefined
+symbol = unique
+
+object
+*/
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
