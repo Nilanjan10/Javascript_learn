6a39c3e95ca7aa650523fbbaed9d32cb0bf6a7ef
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-04-03 22:42:47 +0530

    Add files via upload

---
+++ Basic/Conversion.js
@@ -0,0 +1,36 @@
+/*let score = 33
+console.log(typeof score); //number
+*/
+/*CONVERTING STRING TO NUMBER
+let score = "33"
+console.log(typeof score); //string
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);//number
+console.log(scoreInNumber);//33
+
+let sc = "33aB" 
+console.log(typeof sc); //string
+let scInNumber = Number(sc)
+console.log(typeof scInNumber);//number
+console.log(scInNumber);//NaN(not an no.) =>same for let sc = undefined
+*/
+/*CONVERTING NULL TO NUMBER
+let n = null
+console.log(typeof n);//object
+let nInNumber = Number(n)
+console.log(typeof nInNumber);//number
+console.log(nInNumber);//0
+*/
+// CONVERTING BOOLEAN TO NUMBER
+let n=true
+console.log(typeof n);//boolean
+let nInNum = Number(n)
+console.log(typeof nInNum);//number
+console.log(nInNum);//1
+
+/*CONVERTING TO BOOLEAN
+1=TRUE,0=FALSE
+""=FALSE
+"NIL"=TRUE
+*/
+//NO. TO STRING CONVERSION IS ALSO POSSIBLE
