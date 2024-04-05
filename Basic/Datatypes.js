f2fc811aa225977fa97292cea57a82664129b7d8
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-04-05 20:26:55 +0530

    Update Datatypes.js

--- Basic/Datatypes.js
+++ Basic/Datatypes.js
@@ -22,3 +22,18 @@ object
 
 console.log(typeof undefined);//undefined
 console.log(typeof null);//object
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
