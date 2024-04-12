7dca5135d488283ba3c71226d0f8d34ce99ce064
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-04-13 00:23:24 +0530

    Update Maths.js

--- Basic/Maths.js
+++ Basic/Maths.js
@@ -1,1 +1,13 @@
+/*
+console.log(Math.abs(-4));//4
+console.log(Math.round(4.6));//5
+console.log(Math.ceil(4.6));//5
+console.log(Math.floor(4.6));//4
+console.log(Math.min(4,6,1,99,8.2));//1
+console.log(Math.max(4,6,1,8.9,99,9));//99
+*/
+console.log(Math.random());//0.5300970451572065//value b/w 0-1
 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min +1)+min));//random no. b/w 10-20
