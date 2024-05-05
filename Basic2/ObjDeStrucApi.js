da47427b6fe20e825fcd7b04a9f526c5505df782
Nilanjan Bhattacharya <97098504+Nilanjan10@users.noreply.github.com>
2024-05-05 19:22:22 +0530

    Add files via upload

---
+++ Basic2/ObjDeStrucApi.js
@@ -0,0 +1,20 @@
+const course = {
+    courseName:"javascript",
+    coursePrice:"999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor:instructor} = course
+console.log(instructor);//hitesh
+
+//APIs in json format
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
